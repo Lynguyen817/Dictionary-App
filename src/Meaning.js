0f8adb01f9274dcde97571ecda422d5b78192e5c
Lynguyen817 <lynguyen1708@gmail.com>
2022-02-10 16:47:49 -0500

    update meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,16 +4,20 @@ export default function Meaning(props){
     console.log(props.meaning);
     return (
       <div className="Meaning">
-        <h4><em>noun</em></h4>
-          <h3> {props.meaning.partOfSpeed} </h3>
+          <h4> {props.meaning.partOfSpeed} </h4>
           {props.meaning.definitions.map(function(definition,index){
+              
               return(
                   <div key={index}>
                       <ul>
                           <li>{definition.definition}</li>
                           <br/>
                           <em>Example: {definition.example}</em>
-                          
+                          <br/>
+                          <div><em>Synonyms</em>: {definition.synonyms.map(function(synonym,index){
+                              return( <span key={index}> {synonym}, </span>)
+                          })}
+                          </div>
                       </ul>
                   </div>
               );
