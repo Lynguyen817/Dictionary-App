74d82f2efeb8bc32569aa9c9ad05e035e4473f69
Lynguyen817 <lynguyen1708@gmail.com>
2022-02-10 16:12:29 -0500

    fix rendering meaning

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,7 +4,7 @@ export default function Meaning(props){
     console.log(props.meaning);
     return (
       <div className="Meaning">
-          <h3><em>noun</em></h3>
+        <h4><em>noun</em></h4>
           <h3> {props.meaning.partOfSpeed} </h3>
           {props.meaning.definitions.map(function(definition,index){
               return(
